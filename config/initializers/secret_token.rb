66f1522cdfef87cee57b4133ee837c512dd518cb
Channa Ly <channa.info@gmail.com>
2013-06-03 09:05:07 +0700

    init png project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PngHivAids::Application.config.secret_token = 'cd066a9fc4a46e19afbebd56848cb19624de3e0a100ac54c0432d5f1daed8b638b711fa6bdb5a595615d0d8791233b866225bb7528cb0e3df3c42fcfd7edf599'
