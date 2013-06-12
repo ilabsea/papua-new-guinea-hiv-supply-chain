d84da5804c2e80d3a2c041f6622cf3797e07a26a
Reaksmey <reaksmey@instedd.org>
2013-06-12 10:28:54 +0700

    move commodity to admin

---
+++ app/controllers/admin/commodities_controller.rb
@@ -0,0 +1,61 @@
+module Admin
+  class CommoditiesController < ApplicationController
+    # GET /commodities
+    # GET /commodities.json
+    def index
+      @commodities = Commodity.all
+
+      respond_to do |format|
+        format.html # index.html.erb
+        format.json { render json: @commodities }
+      end
+    end
+
+    # GET /commodities/1
+    # GET /commodities/1.json
+    def show
+      @commodity = Commodity.find(params[:id])
+    end
+
+    # GET /commodities/new
+    # GET /commodities/new.json
+    def new
+      @commodity = Commodity.new
+    end
+
+    # GET /commodities/1/edit
+    def edit
+      @commodity = Commodity.find(params[:id])
+    end
+
+    # POST /commodities
+    # POST /commodities.json
+    def create
+      @commodity = Commodity.new(params[:commodity])
+      if @commodity.save
+        redirect_to admin_commodity_path(@commodity), notice: 'Commodity was successfully created.'
+      else
+        render action: "new" 
+      end
+    end
+
+    # PUT /commodities/1
+    # PUT /commodities/1.json
+    def update
+      @commodity = Commodity.find(params[:id])
+      if @commodity.update_attributes(params[:commodity])
+        redirect_to admin_commodity_path(@commodity), notice: 'Commodity was successfully updated.' 
+      else
+        render action: "edit" 
+      end
+    end
+
+    # DELETE /commodities/1
+    # DELETE /commodities/1.json
+    def destroy
+      @commodity = Commodity.find(params[:id])
+      @commodity.destroy
+      redirect_to admin_commodities_url 
+    end
+  end
+end
--- app/models/commodity.rb
+++ app/models/commodity.rb
@@ -2,4 +2,6 @@ class Commodity < ActiveRecord::Base
   attr_accessible :commodity_category_id, :consumption_per_client_pack, :consumption_per_client_unit, :name
 
   belongs_to :commodity_category
+
+  validates :commodity_category_id, :consumption_per_client_pack, :consumption_per_client_unit, :name , :presence   =>  true
 end
