8ded74441d082b5da63c1bd5e8d7387da94c2072
Channa Ly <channa.info@gmail.com>
2013-07-04 17:38:43 +0700

    data-entry dashboard

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -3,11 +3,15 @@ module Admin
   	def index
   		if current_user.admin?
   			redirect_to admin_settings_path()
-			elsif current_user.data_entry?
-				redirect_to admin_orders_path()	
-			else
-				redirect_to admin_requisition_reports_path()			
-			end
+		elsif current_user.data_entry?
+			render :data_entry
+		else
+			redirect_to admin_requisition_reports_path()			
 		end
 	end
+
+	def data_entry
+
+	end
+  end
 end
