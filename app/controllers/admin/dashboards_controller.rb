0e499a1e5ce9f0d5a91fe62ed37b85128bab8eb3
Reaksmey <reaksmey@instedd.org>
2013-07-04 16:34:49 +0700

    change top menu bar for site_user

--- app/controllers/admin/dashboards_controller.rb
+++ app/controllers/admin/dashboards_controller.rb
@@ -4,7 +4,9 @@ module Admin
   		if current_user.admin?
   			redirect_to admin_settings_path()
 			elsif current_user.data_entry?
-				redirect_to admin_orders_path()				
+				redirect_to admin_orders_path()	
+			else
+				redirect_to admin_requisition_reports_path()			
 			end
 		end
 	end
