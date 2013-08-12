ed4c614bd440a9b904389f3085483a461f6d5b56
Channa Ly <channa.info@gmail.com>
2013-08-12 16:57:22 +0700

    remove email index from user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130808102656) do
+ActiveRecord::Schema.define(:version => 20130812095404) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -224,7 +224,6 @@ ActiveRecord::Schema.define(:version => 20130808102656) do
     t.string   "role"
   end
 
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["phone_number"], :name => "index_users_on_phone_number", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   add_index "users", ["user_name"], :name => "index_users_on_user_name", :unique => true
